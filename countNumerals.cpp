9df79ae1a35c81086d99ee8b4076ff405b0fcea5
pbegun <paigebegun@gmail.com>
2018-02-22 11:45:31 -0800

    Switched from spaces to tabs

--- countNumerals.cpp
+++ countNumerals.cpp
@@ -6,34 +6,34 @@ static const int TEN = 10;
 
 int main(int argc, char* argv[])
 {
-  int* nums = new int[TEN];
-  int lower = 0, upper = 0;
+	int* nums = new int[TEN];
+	int lower = 0, upper = 0;
 
-  // Get user input
-  cout << "Give a lower bound (must be greater than 0): ";
-  cin >> lower;
-  cout << "Give an upper bound: ";
-  cin >> upper;
+	// Get user input
+	cout << "Give a lower bound (must be greater than 0): ";
+	cin >> lower;
+	cout << "Give an upper bound: ";
+	cin >> upper;
 
-  // Initialize nums[]
-  for (int i = ZERO; i < TEN; i++)
-    nums[i] = ZERO;
+	// Initialize nums[]
+	for (int i = ZERO; i < TEN; i++)
+		nums[i] = ZERO;
 
-  // Count digits
-  for (int i = lower; i <= upper; i++)
-  {
-    int j = i;
+	// Count digits
+	for (int i = lower; i <= upper; i++)
+	{
+		int j = i;
 
-    while (j != ZERO)
-    {
-      nums[j % TEN]++;
-      j = j / TEN;
-    } // while
-  } // for
+		while (j != ZERO)
+		{
+			nums[j % TEN]++;
+			j = j / TEN;
+		} // while
+	} // for
 
-  // Print output
-  for (int i = ZERO; i < TEN; i++)
-    cout << i << ": " << nums[i] << endl;
+	// Print output
+	for (int i = ZERO; i < TEN; i++)
+		cout << i << ": " << nums[i] << endl;
 	
-  return ZERO;
+	return ZERO;
 } // main()
